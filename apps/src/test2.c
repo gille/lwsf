3f1d6a6dd14b51ac14617c20fcfa8c3cde04c593
Magnus Gille <mgille@gmail.com>
2011-12-14 01:20:55 +0000

    first commit

---
+++ apps/src/test2.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include "lwsf.h"
+#if 0
+#define printd(fmt, args...)
+#else
+#define printd(fmt, args...) { printf("[%s:%d] "fmt, __FILE__, __LINE__, ##args); }
+#endif
+
+
+void echo(void *arg) {
+  void *m;
+  for(;;) {
+    printd("Waiting for message\n");
+    m = lwsf_msg_recv(NULL);
+    printd("Received message!\n");
+    lwsf_msg_send(&m, lwsf_msg_sender(m));
+  }
+}
+
+void ping(void *arg) {
+  struct th *t = arg;
+  int i;
+  uint32_t *m;
+  m = lwsf_msg_alloc(10, 10);
+  for(i=0; i < 5; i++) {
+    lwsf_msg_send(&m, t);
+    m = lwsf_msg_recv(NULL);
+    if(*m != 10) {
+      printd("wrong message number\n");
+      exit(1);
+    }
+  }
+  exit(0);
+}
+
+void handler1(void) {
+  struct th *t;
+  t = new_thread("p0", echo, NULL);
+  new_thread("p1", ping, t);
+}
+
+int main(void) {
+  lwsf_start(NULL, handler1);
+  return 0;
+}
