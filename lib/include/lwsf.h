ae1b5b9b7225ba711743b8b4b54c20c7924e8cea
Magnus Gille <mgille@gmail.com>
2011-12-15 06:24:54 +0000

    added externs and some cleanups

--- lib/include/lwsf.h
+++ lib/include/lwsf.h
@@ -6,21 +6,22 @@ struct lwsf_list;
 struct lwsf_msg_queue;
 
 typedef struct lwsf_msg_queue lwsf_msg_queue;
+typedef struct lwsf_th lwsf_th;
 
-void lwsf_start(void (*handler0)(void), void (*handler1)(void));
+extern void lwsf_start(void (*handler0)(void), void (*handler1)(void));
 
-struct lwsf_th* lwsf_thread_new(const char *name, void (*entry)(void*), void *arg);
-void lwsf_thread_start(struct lwsf_th *t);
-void lwsf_thread_yield(void);
-void lwsf_thread_stop(struct lwsf_th *t);
+extern lwsf_th* lwsf_thread_new(const char *name, void (*entry)(void*), void *arg);
+extern void lwsf_thread_start(lwsf_th *t);
+extern void lwsf_thread_yield(void);
+extern void lwsf_thread_stop(lwsf_th *t);
 
-void lwsf_msg_send(void **_m, struct lwsf_th *t);
-void lwsf_msg_sendq(void **_m, lwsf_msg_queue *t);
-void * lwsf_msg_recv(lwsf_msg_queue * m);
-void *lwsf_msg_recv_try(lwsf_msg_queue *m);
-struct lwsf_th* lwsf_msg_sender(void *m);
-void *lwsf_msg_alloc(int size, int id);
+extern void lwsf_msg_send(void **_m, lwsf_th *t);
+extern void lwsf_msg_sendq(void **_m, lwsf_msg_queue *t);
+extern void * lwsf_msg_recv(lwsf_msg_queue * m);
+extern void *lwsf_msg_recv_try(lwsf_msg_queue *m);
+extern lwsf_th* lwsf_msg_sender(void *m);
+extern void *lwsf_msg_alloc(int size, int id);
 
-lwsf_msg_queue* lwsf_msgq_create();
+extern lwsf_msg_queue* lwsf_msgq_create(void);
 
 #endif
--- lib/src/include/lwsf_internal.h
+++ lib/src/include/lwsf_internal.h
@@ -2,7 +2,7 @@
 #define LWSF_INTERNAL_H
 
 extern void * lwsf_arch_create_context(int size);
-extern void lwsf_arch_swap_thread(void *out, void *in); 
+extern void lwsf_arch_thread_swap(void *out, void *in); 
 extern void lwsf_thread_entry(void);
 
 #endif
