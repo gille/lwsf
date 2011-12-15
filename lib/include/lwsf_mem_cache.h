b607c67f74db3113ac6b6a19567d9f9973ed481c
Magnus Gille <mgille@gmail.com>
2011-12-15 06:56:39 +0000

    added header

---
+++ lib/include/lwsf_mem_cache.h
@@ -0,0 +1,12 @@
+#ifndef LWSF_MEM_CACHE_H
+#define LWSF_MEM_CACHE_H
+
+struct lwsf_mem_cache;
+typedef struct lwsf_mem_cache lwsf_mem_cache;
+
+extern void lwsf_mem_cache_destroy(lwsf_mem_cache *c);
+extern lwsf_mem_cache * lwsf_mem_cache_create(int block_size);
+extern void *lwsf_mem_cache_alloc(struct lwsf_mem_cache *c);
+extern void lwsf_mem_cache_free(void *m);
+
+#endif
